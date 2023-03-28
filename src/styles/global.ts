6e92ce657e7007468bb3f244c51721e802f83350
joao-hideki <joaoohideki12345@gmail.com>
2023-03-28 03:54:48 -0300

    configurando styled-components e setando estilo global

---
+++ src/styles/global.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+  }
+
+  body {
+    background-color: #C4C4CC;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`;
